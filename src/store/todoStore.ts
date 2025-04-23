2468745c96acdadd93f73d508703d0dd69136782
fromagetriste __ <fromagetriste@gmail.com>
2025-04-23 14:44:05 -0600

    filled the zustand store

---
+++ src/store/todoStore.ts
@@ -0,0 +1,45 @@
+import { create } from "zustand";
+
+type Todo = {
+  id: number;
+  text: string;
+  completed: boolean;
+};
+
+type TodoStore = {
+  todos: Todo[];
+  addTodo: (text: string) => void;
+  toggleTodo: (id: number) => void;
+  deleteTodo: (id: number) => void;
+  clearTodos: () => void;
+};
+
+export const useTodoStore = create<TodoStore>((set) => ({
+  todos: [],
+
+  addTodo: (text) =>
+    set((state) => ({
+      todos: [
+        ...state.todos,
+        {
+          id: Date.now(),
+          text,
+          completed: false,
+        },
+      ],
+    })),
+
+  toggleTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      ),
+    })),
+
+  deleteTodo: (id) =>
+    set((state) => ({
+      todos: state.todos.filter((todo) => todo.id !== id),
+    })),
+
+  clearTodos: () => set({ todos: [] }),
+}));
